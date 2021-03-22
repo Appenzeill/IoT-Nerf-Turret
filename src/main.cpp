d1336fe5867a2710317ef8465a1695b29c47c8c2
Appenzeill <Daanroth@protonmail.com>
2021-03-22 09:37:14 +0100

    Cmake werkt nu goed samen met WiringPi, Servo control werkt op main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,16 @@
-#include <iostream>
-#include <opencv2/core.hpp>
-
-using namespace std;
-
-int main() {
-  cout << "Placeholder code \n";
+#include<wiringPi.h>
+ 
+int main(void)
+{
+    wiringPiSetupGpio();
+    pinMode(18,PWM_OUTPUT);
+    pwmSetMode(PWM_MODE_MS);
+    pwmSetRange(2000);
+    pwmSetClock(192);
+     
+    while(1)
+    {
+    pwmWrite(18,180);
+    }
+    return 0;
 }
-
