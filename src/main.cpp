8d75916e0953e0ae24209834c3ae4a2f6d112782
Appenzeill <Daanroth@protonmail.com>
2021-03-22 10:58:54 +0100

    2 servos werken nu

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,31 @@
-#include<wiringPi.h>
- 
-int main(void)
-{
-    wiringPiSetupGpio();
-    pinMode(18,PWM_OUTPUT);
-    pwmSetMode(PWM_MODE_MS);
-    pwmSetRange(2000);
-    pwmSetClock(192);
-     
-    while(1)
-    {
-    pwmWrite(18,180);
-    }
-    return 0;
+#include <wiringPi.h>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  string servo_axis;
+  int servo_degrees, pin;
+  cout << "Axis: (xy/z) ";
+  cin >> servo_axis;
+  cout << "Servo graden: ";
+  cin >> servo_degrees;
+
+  wiringPiSetupGpio();
+  if (servo_axis == "xy") {
+    pinMode(18,PWM_OUTPUT); // 18
+    pin = 18;
+  } else {
+    pinMode(19,PWM_OUTPUT); // 19
+    pin = 19;
+    
+  }
+  pwmSetMode(PWM_MODE_MS);
+  pwmSetRange(2000);
+  pwmSetClock(192);
+  
+  while(1) {
+    pwmWrite(pin,servo_degrees); // 18, 50
+  }
+  return 0;
 }
