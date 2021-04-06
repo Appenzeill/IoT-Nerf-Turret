01c513315e5b86a702667f192db542f1ad97bb9e
Appenzeill <Daanroth@protonmail.com>
2021-04-06 17:32:32 +0200

    /build/ verbeterd.

---
+++ build/CMakeFiles/controller.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/controller.dir/src/controller/PracticalSocket.cpp.o"
+  "CMakeFiles/controller.dir/src/controller/main.cpp.o"
+  "controller"
+  "controller.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/controller.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/turret.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/turret.dir/src/turret/PracticalSocket.cpp.o"
+  "CMakeFiles/turret.dir/src/turret/main.cpp.o"
+  "turret"
+  "turret.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/turret.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
