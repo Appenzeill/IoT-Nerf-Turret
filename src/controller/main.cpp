1da12439f39f02d3d87396a21110c8c6b1e61cd9
rick2003 <rick.nuhaan@student.hu.nl>
2021-04-07 15:14:45 +0200

    toevoeging controller

--- src/controller/main.cpp
+++ src/controller/main.cpp
@@ -1,147 +1,97 @@
+#include <iostream>
 #include <wiringPi.h>
+#include <pthread.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include "pubsub.h"
-#include <iostream>
-
+#include <string>
+#include <vector>
 using namespace std;
 
-bool fire = false;
+#define L 27
+#define R 17
+#define S 16
+#define Blue 21
+#define Red 20
+#define Yellow 26
 
-int horizontal_new = 130;
-int horizontal_old = 130;
-int horizontal_min = 30;
-int horizontal_max = 260;
-
-int vertical_min = 80;
-int vertical_max = 160;
-int vertical_new = 120;
-int vertical_old = 120;
-
-// handmatige implementatie van pwm signal.
-PI_THREAD(fire_servo) {
-  while (true) {
-    if (fire == false) {
-      for (int i = 0; i <= 50; i++) {
-	usleep(19500);
-	digitalWrite (26, HIGH);
-	usleep(500);
-	digitalWrite (26,  LOW);
-      }
-    } else {
-      for (int i = 0; i <= 50; i++) {
-	usleep(17500);
-	digitalWrite (26, HIGH);
-	usleep(2500);
-	digitalWrite (26,  LOW);
-      }
+void setup(){
+    wiringPiSetupGpio();
+    pinMode(L, INPUT);
+    pinMode(R, INPUT);
+    pinMode(S, INPUT);
+    pinMode(Blue, OUTPUT);
+    pinMode(Red, OUTPUT); 
+    pinMode(Yellow, OUTPUT);
+}
+void leds(vector<int> values = {0,0,0}){
+    if(values[2] == 1){
+        digitalWrite(Blue, HIGH);
+    }else{
+        digitalWrite(Blue, LOW);
     }
-  }
-  return(0);
+    if(values[1] == 1){
+        digitalWrite(Red, HIGH);
+    }else{
+        digitalWrite(Red, LOW);
+    }
+    if(values[0] == 1){
+            digitalWrite(Yellow, HIGH);
+    }else{
+        digitalWrite(Yellow, LOW);
+    }
 }
 
-PI_THREAD(vertical_servo) {
-  while (true) {
-    usleep(30000);
-    if (horizontal_new != vertical_old && vertical_new < vertical_old) {
-      for (int i = vertical_old; i >= vertical_new; i--) {
-	usleep(10000);
-	pwmWrite(19,i); // 18, 50
-      }
-    } else if (vertical_new != vertical_old && vertical_new > vertical_old) {
-      for (int i = vertical_old; i <= vertical_new; i++) {
-	usleep(10000);
-	pwmWrite(19,i); // 18, 50
-      }
+void knoppen(bool keuze){
+    publisher p("Turret_controller7");
+    while(!digitalRead(S)){
+        if(keuze == true){
+            if(digitalRead(L)){
+                cout << "Omhoog +=1" << endl; 
+                p.send("omhoog");
+                delay(200);
+            }else if(digitalRead(R)){
+                cout << "omlaag +=1" << endl;
+                p.send("omlaag");
+                delay(200);
+            }
+        }else{
+            if(digitalRead(L)){
+                cout << "Links +=1" << endl;
+                p.send("links");
+                delay(200);
+            }else if(digitalRead(R)){
+                cout << "rechts +=1" << endl;
+                p.send("rechts");
+                delay(200);
+            }
+        }
     }
-    vertical_old = vertical_new;
-  }
-  return(0);
+    delay(200);
+    cout << "Terug naar keuzes" << endl;
 }
-
-PI_THREAD(horizontal_servo) {
-  while (true) {
-    usleep(30000);
-    if (horizontal_new != horizontal_old && horizontal_new < horizontal_old) {
-      //cout << "kleiner \n";
-      for (int i = horizontal_old; i >= horizontal_new; i--) {
-	usleep(10000);
-	pwmWrite(18,i); // 18, 50
-      }
-    } else if (horizontal_new != horizontal_old && horizontal_new > horizontal_old) {
-      //cout << "groter \n";
-      for (int i = horizontal_old; i <= horizontal_new; i++) {
-	usleep(10000);
-	pwmWrite(18,i); // 18, 50
-      }
+int main(void){
+    setup();
+    cout << "De keuzes voor de controller zijn \nKnop 1 links/rechts, Knop 2 omhoog/omlaag, knop 3 schieten."<< endl;
+    while(true){
+        leds({0,0,1});
+        if(digitalRead(L)){
+            cout << "Druk op knop 1 om naar links te draaien, druk op knop 2 om naar rechts te draaien, knop 3 voor terug te gaan" << endl;
+            delay(200);
+            leds({1,0,0});
+            knoppen(0);
+        }else if(digitalRead(R)){
+            cout << "Druk op knop 1 om naar omhoog te draaien, druk op knop 2 om naar omlaag te draaien, knop 3 voor terug te gaan" << endl;
+            delay(200);
+            leds({0,1,0});
+            knoppen(1);
+        }else if(digitalRead(S)){
+            delay(200);
+            leds({0,0,0});
+            cout << "Geschoten!!!!" << endl;
+            publisher p("Turret_controller7");
+            p.send("schiet");
+            exit(0);
+        }
     }
-    horizontal_old = horizontal_new;
-  }
-  return(0);
-}
-
-void onrecieve(string message) {
-  int interval = 5;
-  if (message == "schiet") {
-    if (fire == false) {
-      fire = true;
-    } else {
-      fire = false;
-    }
-  } else if (message == "omlaag") {
-    if ((vertical_new + 10) <= vertical_max) {
-      vertical_old = vertical_new;
-      vertical_new = vertical_new + interval;
-    }
-  } else if (message == "omhoog") {
-    if ((vertical_new - 10) >= vertical_min) {
-      vertical_old = vertical_new;
-      vertical_new = vertical_new - interval;
-    }
-  } else if (message == "links") {
-    if ((horizontal_new + 10) <= horizontal_max) {
-      horizontal_old = horizontal_new;
-      horizontal_new = horizontal_new + interval;
-    }
-  } else if (message == "rechts") {
-    if ((horizontal_new - 10) >= horizontal_min) {
-      horizontal_old = horizontal_new;
-      horizontal_new = horizontal_new - interval;
-    }
-  }
-}
-
-int main(void) {
-  // initialiseer de threads.
-  int t0 = piThreadCreate(vertical_servo);
-  int t1 = piThreadCreate(horizontal_servo);
-  int t2 = piThreadCreate(fire_servo);
-
-  // hoeveel punten per keer met draaien.
-  int interval = 5;
-
-  // error handling van de threads
-  if (t0 != 0 || t1 != 0 || t2 != 0 ) {
-    std::cout << "threads did not start correctly." << std::endl;
-  }
-  
-  wiringPiSetupGpio();
-  // schiet servo
-  pinMode (26, OUTPUT);
-
-  // pwm servos
-  pinMode(18,PWM_OUTPUT); // 18
-  pinMode(19,PWM_OUTPUT); // 19
-  
-  pwmSetMode(PWM_MODE_MS);
-  pwmSetRange(2000);
-  pwmSetClock(192);
-
-  // pubsub configuratie
-  subscription s("Turret_controller7", onrecieve);
-
-  cout << "waiting for messages" << endl;
-  cout << "druk op de \'s\' knop om te stoppen"  << endl;
-  cin.get();
-
-  return(0);
 }
