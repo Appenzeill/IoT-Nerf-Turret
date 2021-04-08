358ffaad05d2bc35cd605157e3627c209bff3597
Ray <74265433+Rayax10@users.noreply.github.com>
2021-04-08 14:39:21 +0200

    update main.cpp

--- src/controller/main.cpp
+++ src/controller/main.cpp
@@ -16,6 +16,7 @@ using namespace std;
 #define Yellow 26
 
 void setup(){
+    //Alle leds worden hier op de juiste mode gezet dus input/output
     wiringPiSetupGpio();
     pinMode(L, INPUT);
     pinMode(R, INPUT);
@@ -25,6 +26,7 @@ void setup(){
     pinMode(Yellow, OUTPUT);
 }
 void leds(vector<int> values = {0,0,0}){
+    //indicator leds voor welke mode de gebruiker zich bevindt. 
     if(values[2] == 1){
         digitalWrite(Blue, HIGH);
     }else{
@@ -45,8 +47,8 @@ void leds(vector<int> values = {0,0,0}){
 void knoppen(bool keuze){
     publisher p("Turret_controller7");
     while(!digitalRead(S)){
-        if(keuze == true){
-            if(digitalRead(L)){
+        if(keuze == true){ //keuze is verticaal
+            if(digitalRead(L)){  
                 //cout << "Omhoog +=1" << endl; 
                 p.send("omhoog");
                 delay(200);
@@ -55,7 +57,7 @@ void knoppen(bool keuze){
                 p.send("omlaag");
                 delay(200);
             }
-        }else{
+        }else{ //keuze is horizontaal
             if(digitalRead(L)){
                 //cout << "Links +=1" << endl;
                 p.send("links");
@@ -68,24 +70,24 @@ void knoppen(bool keuze){
         }
     }
     delay(200);
-    cout << "Terug naar keuzes" << endl;
+    cout << "Terug naar keuzes" << endl; //terug knop wordt ingedrukt zodat de gebruiker weer naar het start-'scherm' kan. 
 }
 int main(void){
     setup();
     cout << "De keuzes voor de controller zijn \nKnop 1 links/rechts, Knop 2 omhoog/omlaag, knop 3 schieten."<< endl;
     while(true){
-        leds({0,0,1});
-        if(digitalRead(L)){
+        leds({0,0,1}); //leds staan op start mode
+        if(digitalRead(L)){ //keuze is horizontaal, leds worden aangepast naar H, en de keuzes worden meegegeven aan de knoppen functie voor verder gebruik
             cout << "Druk op knop 1 om naar links te draaien, druk op knop 2 om naar rechts te draaien, knop 3 voor terug te gaan" << endl;
             delay(200);
             leds({1,0,0});
             knoppen(0);
-        }else if(digitalRead(R)){
+        }else if(digitalRead(R)){ //keuze is verticaal, leds worden aangepast naar V, knoppen functie wordt aangeroepen met de juiste parameters
             cout << "Druk op knop 1 om naar omhoog te draaien, druk op knop 2 om naar omlaag te draaien, knop 3 voor terug te gaan" << endl;
             delay(200);
             leds({0,1,0});
             knoppen(1);
-        }else if(digitalRead(S)){
+        }else if(digitalRead(S)){ //schiet keuze, alle leds gaan uit, en het programma eindigt. Hier wordt een extra delay toegevoegd voor als de terugknop in andere modes te lang word ingedrukt.
             delay(200);
             leds({0,0,0});
             cout << "Geschoten!!!!" << endl;
